8f561bbb25279f59deafb1788f4e6d8cfec8530c
Kai <ali.rf.kashani@gmail.com>
2020-12-15 17:55:39 -0800

    webpack babel sass template

---
+++ app/assets/scripts/modules/test.component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function AppComponent() {
+	return (
+		<>
+			<h1 className='section-title section-title--blue'>
+				This is a react example
+			</h1>
+			<p>Sky is blue, grass is green.</p>
+		</>
+	);
+}
+
+export default AppComponent;
